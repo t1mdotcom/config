3530ed442a0247dfa8b09f76e839575cf7c8da36
Tim Heinemann <tim.heinemann@clintworld.de>
2023-12-10 22:45:32 +0100

    some changes

---
+++ aliases.sh
@@ -0,0 +1,23 @@
+# sudo
+alias _="sudo"
+alias fucking="sudo"
+alias damn="sudo !!"
+
+# docker
+alias cocker="docker compose"
+alias dc="docker compose"
+
+# ls
+alias l='ls -CF'
+alias la='ls -A'
+alias ll='ls -lrt'
+alias ls='ls --color=auto'
+
+# rm
+alias rm="rm -i" 
+
+# general
+alias q=exit
+alias c=clear
+alias update="sudo nala update && sudo nala upgrade"
+#alias threads='ps -eo nlwp | tail -n +2 | awk "{ num_threads += $1 } END { print num_threads }"'
